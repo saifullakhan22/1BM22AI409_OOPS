f48ba402621812db1e9e9b639d25fbe736c2f51a
saifullakhan22 <138455445+saifullakhan22@users.noreply.github.com>
2023-11-19 12:08:07 +0530

    Create password.py

---
+++ password.py
@@ -0,0 +1,13 @@
+def is_Valid(email):
+  if '@' in email and '@' != email[0] and '@' != email[-1]:
+    index_of_at = email.find('@')
+    if '.' != email[index_of_at + 1] and '.' != email[-1]:
+      print("valid")
+    else: 
+      print("not_valid")
+  else:
+    print("not valid")
+
+email = "saif@123.com"
+
+is_Valid(email)
