ec44f81c4cd9f82eaf7cffbca87a9a99a8f946e9
saifullakhan22 <138455445+saifullakhan22@users.noreply.github.com>
2023-11-19 12:10:38 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,5 +1,5 @@
 def is_Valid(email):
-  if '@' in email and '@' != email[0] and '@' != email[-1]:
+  if '@' in email and '@' != email[0] and '@' != email[-1] and len(email)<256:
     index_of_at = email.find('@')
     if '.' != email[index_of_at + 1] and '.' != email[-1]:
       print("valid")
@@ -8,6 +8,7 @@ def is_Valid(email):
   else:
     print("not valid")
 
-email = "saif@123.com"
+email = "saif@777.com"
 
 is_Valid(email)
+
